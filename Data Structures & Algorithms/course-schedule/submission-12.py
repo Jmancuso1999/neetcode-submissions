cc179f440e2de3baa367f7862181020ce982fa85
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2026-04-19 19:06:47 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,38 @@
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for nodeA, nodeB in prerequisites:
+            adj_list[nodeA].append(nodeB)
+            indegree[nodeB] += 1
+
+        queue = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+        
+        validCourses = 0
+
+        while queue:
+            currNode = queue.popleft()
+
+            validCourses += 1
+
+            for neighbor in adj_list[currNode]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+            
+        return validCourses == numCourses
+
+# class Solution:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         adj_list[[] for _ in range(numCourses)]
+
+#         for nodeA, nodeB in prerequisites:
+#             adj_list[nodeB].append(nodeA)
+
+            
