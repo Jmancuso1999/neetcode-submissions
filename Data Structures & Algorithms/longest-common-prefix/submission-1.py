ea48cd754abde2d5d5fa91b3d0543c35c9a94fad
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2026-04-19 13:14:45 -0400

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0] 
+
+        # Find smallest word
+        for word in strs:
+            if len(word) < len(prefix):
+                prefix = word
+
+        for currWord in range(1, len(strs)):
+            startPos = 0
+            while startPos < len(prefix):
+                if prefix[startPos] != strs[currWord][startPos]:
+                    break # End's while loop
+                startPos += 1
+
+            # Updates for every word we visit and resets it with next
+            # smallest prefix (if there)
+            prefix = prefix[:startPos] 
+        
+        return prefix
