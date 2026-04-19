d2e2c9dda9aafad3f2ebe38aaf01750e1c6e6cd1
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2026-04-19 19:13:43 -0400

    Add: course-schedule - submission-13

---
+++ Data Structures & Algorithms/course-schedule/submission-13.py
@@ -0,0 +1,65 @@
+"""
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for nodeA, nodeB in prerequisites:
+            adj_list[nodeA].append(nodeB)
+            indegree[nodeB] += 1
+
+        queue = deque()
+
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+        
+        validCourses = 0
+
+        while queue:
+            currNode = queue.popleft()
+
+            validCourses += 1
+
+            for neighbor in adj_list[currNode]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+            
+        return validCourses == numCourses
+"""
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = collections.defaultdict(list)
+
+        for sourceNode, destNode in prerequisites:
+            adj_list[destNode].append(sourceNode)
+        
+        visited = set()
+
+        def dfs(currCourse):
+            if currCourse in visited:
+                return False
+            
+            if adj_list[currCourse] == []:
+                return True
+            
+            visited.add(currCourse)
+            for neighbor in adj_list[currCourse]:
+                if not dfs(neighbor):
+                    return False
+            
+            visited.remove(currCourse)
+
+            # Mark all elements in the hash map list as visited
+            adj_list[currCourse] = []
+
+            return True
+    
+        for course in range (numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
+
+            
