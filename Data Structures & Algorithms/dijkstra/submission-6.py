80b84c859a8f6123332cfbe3faad4387bc52e1fe
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2026-04-22 11:47:32 -0400

    Add: dijkstra - submission-6

---
+++ Data Structures & Algorithms/dijkstra/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shortestPath(self, n: int, edges: List[List[int]], src: int) -> Dict[int, int]:
+        adj_list = {}
+        shortest = {}
+        minHeap = [[0, src]]
+
+        for node in range(n):
+            adj_list[node] = []
+        
+        for nodeU, nodeV, weight in edges:
+            adj_list[nodeU].append([nodeV, weight])
+        
+        while minHeap:
+            currWeight, currNode = heapq.heappop(minHeap)
+
+            if currNode in shortest:
+                continue
+            
+            shortest[currNode] = currWeight
+
+            for neighborNode, neighborWeight in adj_list[currNode]:
+                heapq.heappush(minHeap, [currWeight + neighborWeight, neighborNode])
+        
+        for node in range(n):
+            if node not in shortest:
+                shortest[node] = -1
+        
+        return shortest
