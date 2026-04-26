c9ced536ad93bb609e265da67180cd63ba1997d8
James Mancuso <43454765+Jmancuso1999@users.noreply.github.com>
2026-04-26 00:10:47 -0400

    Add: maximum-product-subarray - submission-9

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-9.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        numLength = len(nums)
+        largestProduct = nums[0]
+        prefix = 1
+        suffix = 1
+
+        for pos in range(numLength):
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+            
+            prefix *= nums[pos]
+            suffix *= nums[numLength - pos - 1]
+
+            largestProduct = max(largestProduct, prefix, suffix)
+        
+        return largestProduct
+"""
+[1,2,-1,4,3,-6,2,-2]
+
+If we see a 0 like [1, 0,-1,4] - turn 0 to 1 using prefix/suffix method
+
+
+"""
+
+"""
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        totalMax = nums[0]
+
+        for i in range(len(nums)):
+            currMax = nums[i] 
+            for j in range(i + 1, len(nums)):
+                totalMax = max(totalMax, currMax)
+                currMax *= nums[j]   
+
+            totalMax = max(totalMax, currMax)
+        
+        return totalMax
+"""
